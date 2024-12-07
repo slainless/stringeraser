6e5edb1965dd80f63239007c4bac723056f363ab
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-07 15:21:40 +0800

    feat: add switches to match pane

--- src/components/match-pane.tsx
+++ src/components/match-pane.tsx
@@ -1,5 +1,14 @@
-import type { Matcher } from "@/core/matcher";
-import { For, Index } from "solid-js";
+import { Matcher } from "@/core/matcher";
+import {
+  createMemo,
+  createSignal,
+  For,
+  Index,
+  type JSX,
+  Match,
+  Switch,
+  useContext,
+} from "solid-js";
 import { Checkbox, CheckboxControl } from "./ui/checkbox";
 import { StoreContext } from "./store";
 import { safeRegex } from "safe-regex2";
@@ -43,29 +52,52 @@ export function MatchPane() {
   return (
     <div class="p-5 h-[inherit] w-md overflow-y-auto border-r border-r-solid border-r-slate-2">
       <div class="flex flex-col gap-row-2">
-        <Index each={matches}>
-          {(item) => {
-            return (
-              <div class="grid grid-cols-[max-content_auto] gap-col-2 w-full">
-                <div class="flex items-center justify-center">
-                  <Checkbox>
-                    <CheckboxControl />
-                  </Checkbox>
-                </div>
-                <div>
-                  <div>
-                    <span>abracadabra is magical.</span>
+        <Switch>
+          <Match when={!isEmpty(errors())}>
+            <div>{errors()}</div>
+          </Match>
+          <Match
+            when={
+              isEmpty(store.text) ||
+              (isEmpty(store.lookup.strings) && isEmpty(filteredRegexps()))
+            }
+          >
+            <div class="text-foreground/30">
+              Fill text & lookup pattern first
+            </div>
+          </Match>
+          <Match when={matches().length < 1}>
+            <div class="text-foreground/30">No matching pattern found</div>
+          </Match>
+          <Match when={matches()}>
+            <Index each={matches()}>
+              {(item) => {
+                return (
+                  <div class="grid grid-cols-[max-content_auto] gap-col-2 w-full">
+                    <div class="flex items-center justify-center">
+                      <Checkbox>
+                        <CheckboxControl />
+                      </Checkbox>
+                    </div>
+                    <div>
+                      <div>
+                        <span>abracadabra is magical.</span>
+                      </div>
+                      <div class="text-xs">
+                        <span>Matched with: </span>
+                        <span>{item().match}</span>
+                      </div>
+                    </div>
                   </div>
-                  <div class="text-xs">
-                    <span>Matched with: </span>
-                    <span>{item().match}</span>
-                  </div>
-                </div>
-              </div>
-            );
-          }}
-        </Index>
+                );
+              }}
+            </Index>
+          </Match>
+        </Switch>
       </div>
     </div>
   );
 }
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+const isEmpty = (v?: any | any[]) => v == null || v === "" || v?.length < 1;
