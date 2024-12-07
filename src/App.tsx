68ccaaaaad9a0f9d19ee07cc255d22e0d096f120
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-07 11:53:35 +0800

    feat: base app foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,21 @@
-import { createSignal } from "solid-js";
-import solidLogo from "./assets/solid.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import "uno.css";
 import "@unocss/reset/sanitize/sanitize.css";
 import "@unocss/reset/sanitize/assets.css";
+import { StoreProvider } from "./components/store-provider";
+import { ToolbarPane } from "./components/toolbar-pane";
+import { OutputPane } from "./components/output-pane";
 
 function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank" rel="noreferrer">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <div class="min-h-[100vh]">
+      <StoreProvider>
+        <div class="sticky grid grid-cols-[max-content_max-content] min-h-[inherit]">
+          <ToolbarPane />
+          <OutputPane />
+        </div>
+      </StoreProvider>
+    </div>
   );
 }
 
