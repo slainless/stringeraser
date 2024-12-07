0248d04edfd0e0f916bacf9412085b2eb98b90cc
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-07 22:24:40 +0800

    feat: add singlefile build option

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import Solid from "vite-plugin-solid";
+import { viteSingleFile as SingleFileBuild } from "vite-plugin-singlefile";
 import { resolve } from "node:path";
 
 export default defineConfig({
-  plugins: [Solid(), UnoCSS()],
+  plugins: [Solid(), UnoCSS(), process.env.SINGLEFILE ? SingleFileBuild() : []],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
