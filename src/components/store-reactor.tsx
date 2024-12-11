1ff6f8a99245db85513d668cca14a5d63e55d9c3
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-12-12 06:45:22 +0800

    feat: add handler for state change to reflect back to lexical

--- src/components/store-reactor.tsx
+++ src/components/store-reactor.tsx
@@ -1,8 +1,18 @@
 import type { ParentProps } from "solid-js";
-import { createEffect, createMemo, useContext } from "solid-js";
+import { createEffect, useContext } from "solid-js";
 import { StoreContext } from "./store";
 import { on } from "solid-js";
 import { Matcher } from "@/core/matcher";
+import {
+  $getRoot,
+  LineBreakNode,
+  ParagraphNode,
+  TextNode,
+  type LexicalEditor,
+  type SerializedLexicalNode,
+} from "lexical";
+import { produce } from "immer";
+import { unwrap } from "solid-js/store";
 
 export interface StoreReactorProps extends ParentProps {}
 export function StoreReactor(props: StoreReactorProps) {
@@ -27,5 +37,59 @@ export function StoreReactor(props: StoreReactorProps) {
     setMatches(matches);
   });
 
+  createEffect(
+    on([() => store.lexicalEditor, () => store.text], async () => {
+      if (store.lexicalEditor == null) return;
+
+      const shouldUpdate = store.lexicalEditor.read(() => {
+        const currentText = $getRoot().getTextContent();
+        return currentText !== store.text;
+      });
+      if (!shouldUpdate) return;
+
+      const nodes = await parse(store.text, store.lexicalEditor);
+      const state = produce(unwrap(store.lexicalState!), (state) => {
+        state.root.children = nodes;
+      });
+      store.lexicalEditor.setEditorState(
+        store.lexicalEditor.parseEditorState(state),
+      );
+    }),
+  );
+
   return <>{props.children}</>;
+}
+
+function parse(
+  text: string,
+  editor: LexicalEditor,
+): Promise<SerializedLexicalNode[]> {
+  return new Promise((res) => {
+    editor.update(() => {
+      if (text === "") {
+        return res([new ParagraphNode().exportJSON()]);
+      }
+
+      const nodes = text
+        .split("\n")
+        .flatMap((text) => {
+          return [new TextNode(text), new LineBreakNode()];
+        })
+        .filter((node, index, array) => {
+          if (node instanceof TextNode && node.__text === "") return false;
+          if (index === array.length - 1 && node instanceof LineBreakNode)
+            return false;
+          return true;
+        })
+        .map((node) => node.exportJSON());
+
+      return res([
+        {
+          ...new ParagraphNode().exportJSON(),
+          // @ts-expect-error
+          children: nodes,
+        },
+      ]);
+    });
+  });
 }
